e707ce12d2c5a5c7caa9acec8fe551fa8f7ceb4f
Spxtrvmll <matvey.agibalov@mail.ru>
2026-05-17 18:36:03 +0700

    clr

--- Strassen.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-void mm_generate(unsigned int* matA, unsigned int* matB, unsigned int* matC, unsigned int M, unsigned int N, unsigned int K);
-
-unsigned int mm_strassen(unsigned int* matA, unsigned int* matB, unsigned int* matC, unsigned int M, unsigned int N, unsigned int K);
-
-void* malloc(size_t size);
-
-void free(void* k);
